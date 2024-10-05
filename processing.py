0df4f11397a983de0def13ac208995cf2c27c4fa
Elizaveta <liza5valikova@gmail.com>
2024-10-05 09:52:58 +0600

    gjc

--- processing.py
+++ processing.py
@@ -7,10 +7,10 @@ df = pd.read_csv(csv_file_path)
 
 df['time'] = df['time'].str.replace(',', '.').astype(float)
 
-
 engine = create_engine('postgresql+psycopg2://postgres:12345@localhost:5432/postgres')
 
 
 df.to_sql('raw_data', engine, if_exists='replace', index=False)
 
 print("Data loaded successfully into PostgreSQL.")
+
